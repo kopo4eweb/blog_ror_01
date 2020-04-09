e2656cfe64d8643be272dbba5192d40478d4c638
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 08:47:54 +0300

    add test "allows a guest to create contact"

--- spec/feature/visitor_create_contact_spec.rb
+++ spec/feature/visitor_create_contact_spec.rb
@@ -5,4 +5,14 @@ feature "Contact creation" do
         visit '/contacts'
         expect(page).to have_content I18n.t('contacts.contact_us')
     end
+
+    scenario "allows a guest to create contact" do
+        visit '/contacts'
+        within(".contact-form") do
+            fill_in 'contact[email]', with: 'user@example.com'
+            fill_in 'contact[message]', with: 'Text message'
+        end
+        click_button 'Send message'
+        expect(page).to have_content 'Thanks!'
+    end
 end
