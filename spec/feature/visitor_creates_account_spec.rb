5eeaf89433b0e945caf4477ac9ad1628dc987b64
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 09:19:56 +0300

    add test "allows guest to create account"

--- spec/feature/visitor_creates_account_spec.rb
+++ spec/feature/visitor_creates_account_spec.rb
@@ -7,4 +7,15 @@ feature "Account creation" do
         expect(page).to have_content I18n.t('devise.sign_up')
     end
 
+    scenario "allows guest to create account" do
+        visit new_user_registration_path
+        fill_in :user_email, with: 'user@example.com'
+        fill_in :user_username, with: 'user'
+        fill_in :user_password, with: 'password'
+        fill_in :user_password_confirmation, with: 'password'
+
+        click_button 'Sign up'
+
+        expect(page).to have_content I18n.t('devise.registrations.signed_up')
+    end
 end
