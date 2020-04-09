7dbb634c8835b01fdb028ae29eb955a533a53154
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 14:10:59 +0300

    refactoring: movie sign up to helper file

--- spec/feature/visitor_creates_account_spec.rb
+++ spec/feature/visitor_creates_account_spec.rb
@@ -8,14 +8,7 @@ feature "Account creation" do
     end
 
     scenario "allows guest to create account" do
-        visit new_user_registration_path
-        fill_in :user_email, with: 'user@example.com'
-        fill_in :user_username, with: 'user'
-        fill_in :user_password, with: 'password'
-        fill_in :user_password_confirmation, with: 'password'
-
-        click_button 'Sign up'
-
+        sign_up
         expect(page).to have_content I18n.t('devise.registrations.signed_up')
     end
 end
