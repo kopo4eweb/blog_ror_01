945a59681b01a1479792f3753a7eb6a645ca3c89
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 18:01:33 +0300

    refactoring  expect text to i18n

--- spec/feature/user_creates_article_spec.rb
+++ spec/feature/user_creates_article_spec.rb
@@ -7,6 +7,6 @@ feature "Article creation" do
 
     scenario "allows user to visit new article page" do
         visit new_article_path
-        expect(page).to have_content 'New Article'
+        expect(page).to have_content I18n.t('articles.new_article')
     end  
 end
