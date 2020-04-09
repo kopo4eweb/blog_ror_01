947ce515297cf417b6f28eb2de89ea746067771e
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 18:48:15 +0300

    test create comments for article

--- spec/feature/user_creates_comment_spec.rb
+++ spec/feature/user_creates_comment_spec.rb
@@ -14,5 +14,19 @@ feature "Comment creation" do
 
         expect(page).to have_content title
         expect(page).to have_css('form.new_comment') 
-    end     
+    end
+
+    scenario "allows user to create comment for article" do
+        visit new_article_path
+        
+        title = 'New article title'
+        text = 'Text article very big!'
+        add_article title, text
+
+        text_comment = 'Text new comments for this articles.'
+        fill_in :comment_body, with: text_comment
+        click_button 'Create Comment'
+
+        expect(page).to have_content  text_comment
+    end  
 end
