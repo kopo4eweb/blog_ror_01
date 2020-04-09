d96f16c8979d98fe19026b20a100f0cdf9b0c9b3
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 18:16:31 +0300

    add test for create new article

--- spec/feature/user_creates_article_spec.rb
+++ spec/feature/user_creates_article_spec.rb
@@ -9,4 +9,17 @@ feature "Article creation" do
         visit new_article_path
         expect(page).to have_content I18n.t('articles.new_article')
     end  
+
+    scenario "allows user to create new article" do
+        visit new_article_path
+
+        title = 'New article title'
+
+        fill_in :article_title, with: title
+        fill_in :article_text, with: 'Text article very big!'
+
+        click_button 'Save Article'
+
+        expect(page).to have_content title
+    end  
 end
