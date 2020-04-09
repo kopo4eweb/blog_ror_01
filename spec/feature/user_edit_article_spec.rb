2542c0f33a6a39b9d0bcdd82e6b6ab1a400c8b39
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 20:26:26 +0300

    test edit article

--- spec/feature/user_edit_article_spec.rb
+++ spec/feature/user_edit_article_spec.rb
@@ -15,4 +15,22 @@ feature "Article edit" do
         
         expect(page).to have_content I18n.t('articles.edit_article')
     end  
+
+    scenario "allows user to edit article" do
+        title = 'New article title'
+        text = 'Text article very big!'
+        add_article title, text
+
+        visit articles_path
+        click_link(I18n.t('articles.link.edit'))
+
+        new_title = 'New article title v2'
+        new_text = 'Text article very big! Edit version.'
+        fill_in :article_title, with: new_title
+        fill_in :article_text, with: new_text
+        click_button 'Save Article'
+        
+        expect(page).to have_content new_title
+        expect(page).to have_content new_text
+    end  
 end
