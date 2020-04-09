d61bbdebb411f1ed4da79e6bc0b84e39ef764157
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-09 09:04:34 +0300

    update label test sign up

--- spec/feature/visitor_creates_account_spec.rb
+++ spec/feature/visitor_creates_account_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "Account creation" do
-    scenario "allows guest to create account" do
+    scenario "allows acceess to contacts page" do
         visit new_user_registration_path
 
         expect(page).to have_content I18n.t('devise.sign_up')
